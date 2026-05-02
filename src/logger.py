e018350819564dc9c94b6df508a400102a0be680
RF-Rezon <rezwanfaysalrezon@gmail.com>
2026-05-02 11:58:43 +0600

    Data Ingestion works with transformation. Next -> implement model trainer.

--- src/logger.py
+++ src/logger.py
@@ -2,16 +2,15 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"   # Example ---->  LOG_FILE = "05_01_2026_13_30_00.log"
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+logs_dir = os.path.join(os.getcwd(), "logs")    # Example ---->  logs_dir = "C:/project/logs"
+os.makedirs(logs_dir, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)    # Example ---->  LOG_FILE_PATH = "C:/project/logs/05_01_2026_13_30_00.log"
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-
-
 )
