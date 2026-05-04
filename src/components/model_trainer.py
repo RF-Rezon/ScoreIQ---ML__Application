b664d0606d3d94e5762249cf6fbe6e4e22bbe3ad
RF-Rezon <rezwanfaysalrezon@gmail.com>
2026-05-05 00:00:37 +0600

    hypter parameter tuning done.

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,6 +18,7 @@ from src.exception import CustomException
 from src.logger import logging
 
 from src.utils import save_object,evaluate_models
+from sklearn.model_selection import GridSearchCV
 
 @dataclass
 class ModelTrainerConfig:
@@ -47,6 +48,46 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
                 "KNeighborsRegressor": KNeighborsRegressor()
             }
+
+            param_grid = {
+                "Random Forest": {
+                    "n_estimators": [50, 100, 200],
+                    "max_depth": [None, 5, 10],
+                    "min_samples_split": [2, 5]
+                },
+
+                "Decision Tree": {
+                    "max_depth": [3, 5, 10, None],
+                    "min_samples_split": [2, 5, 10]
+                },
+
+                "Gradient Boosting": {
+                    "n_estimators": [50, 100, 200],
+                    "learning_rate": [0.01, 0.1, 0.2],
+                    "max_depth": [3, 5]
+                },
+
+                "XGBRegressor": {
+                    "n_estimators": [100, 200],
+                    "learning_rate": [0.05, 0.1],
+                    "max_depth": [3, 5, 7]
+                },
+
+                "KNeighborsRegressor": {
+                    "n_neighbors": [3, 5, 7, 9]
+                },
+
+                "AdaBoost Regressor": {
+                    "n_estimators": [50, 100, 200],
+                    "learning_rate": [0.01, 0.1, 1.0]
+                },
+
+                "CatBoosting Regressor": {
+                    "depth": [4, 6, 8],
+                    "learning_rate": [0.01, 0.1],
+                    "iterations": [100, 200]
+                }
+            }
             
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
@@ -57,26 +98,45 @@ class ModelTrainer:
             best_model_score = model_report[best_model_name]
 
             best_model = models[best_model_name]
+            logging.info(f"Best model: {best_model_name}")
+            # Tuning Part 
 
-            # Check threshold
-            if best_model_score < 0.6:
-                raise CustomException("No best model found")
+            # 👉 Get param for best model
+            params = param_grid.get(best_model_name, {})
 
-            logging.info(f"Found the best model. ->'{best_model_name}'<- and its r2 score is {best_model_score} is the heighst among all the models.")
+            logging.info(f"\n🔧 Hyperparameter tuning started...")
+
+            gs = GridSearchCV(
+                        best_model,
+                        params,
+                        cv=3,
+                        n_jobs=-1,
+                        verbose=2   #  live progress
+                    )
+
+            gs.fit(X_train, y_train)
+
+            logging.info(f"🔥 Best Params: {gs.best_params_}")
+            logging.info(f"📈 CV Best Score: {gs.best_score_:.4f}")
+            tuned_model = gs.best_estimator_
+                    
+            
+            # 👉 Final evaluation with test data.
+            y_pred = tuned_model.predict(X_test)
+            final_score = r2_score(y_test, y_pred)
+
+            logging.info(f"\n🎯 Final Score (after tuning): {final_score:.4f}")
+    
 
             # Save model
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
-                obj=best_model
-            )
-
-            # # Final evaluation
-            # predicted = best_model.predict(X_test)
-            # r2_square = r2_score(y_test, predicted)
-
-            # return best_model_name,best_model_score
+                obj=tuned_model
+                )
             
 
-            
+        
+            return final_score
+
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e,sys)        
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,11 @@
 import os
 import sys
-
-import numpy as np 
-import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+from src.logger import logging
 
 def save_object(file_path, obj):
     try:
@@ -21,21 +20,22 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
     try:
         report = {}
 
         for name, model in models.items():
+            logging.info(f"\n🚀 Training started: {name} (basic).......")
 
             model.fit(X_train, y_train)
-            y_test_pred = model.predict(X_test)
+            y_pred = model.predict(X_test)
 
-            test_model_score = r2_score(y_test, y_test_pred)
+            score = r2_score(y_test, y_pred)
+            report[name] = score
 
-            report[name] = test_model_score
 
-            return report
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
-    
